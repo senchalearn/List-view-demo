cd1162131e38e350ccbae05eaf21c7bad35f9028
Drew Neil <andrew.jr.neil@gmail.com>
2012-02-16 17:36:03 +0100

    Strip Main view down to a container+list.

--- public/app/view/Main.js
+++ public/app/view/Main.js
@@ -14,25 +14,7 @@ Ext.define('ListDemo.view.Main', {
             animation: 'slide'
         },
         items: [
-            {
-                xtype: 'listpanel'
-            },
-            {
-                xtype: 'detailpanel',
-            },
-            {
-                xtype : 'titlebar',
-                docked: 'top',
-                title: 'Bond girls',
-                items: [
-                    {
-                        text: 'back',
-                        ui: 'back',
-                        action: 'back',
-                        hidden: true
-                    }
-                ]
-            }
+            {xtype: 'listpanel'}
         ]
     }
 });
